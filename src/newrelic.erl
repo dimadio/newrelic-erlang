613e5397300203ab40fbb4c9544a4ce21fa3c013
Dmitry Shestak <dmitrys@wix.com>
2013-11-06 15:43:20 +0200

    report correct language

--- src/newrelic.erl
+++ src/newrelic.erl
@@ -58,7 +58,7 @@ connect(Collector, Hostname) ->
               {identifier, app_name()},
               {pid, ?l2i(os:getpid())},
               {environment, []},
-              {language, <<"python">>},
+              {language, <<"erlang">>},
               {settings, {[]}}
              ]}],
 
